4bba4cee38b1f8c2eec74053869cc0e793f044b2
nao370 <kilabasketball@yahoo.co.jp>
2018-03-07 01:05:15 +0900

    roll a ball
    part1まで終了

---
+++ Assets/Scripts/PlayerContoroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoroller : MonoBehaviour {
+
+    public float speed;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
